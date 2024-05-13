31de0a47b984ce62f5f4d8d6adfeb7ca37d3334e
Dilshan Kavinda <kavindakgd@gmail.com>
2024-05-13 23:28:27 +0530

    updating test

--- app/src/main/java/com/example/labexam04/BottomNavBar.kt
+++ app/src/main/java/com/example/labexam04/BottomNavBar.kt
@@ -8,29 +8,12 @@ import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BottomNavBar : AppCompatActivity() {
-    //private lateinit var binding: ActivityBottomNavBarBinding
-    //@SuppressLint("RestrictedApi")
     private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//       binding = ActivityBottomNavBarBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//        replaceFragment(HomeFragment())
-//
-//        binding.bottomNavigationView.setOnItemReselectedListener {
-//            when(it.itemId){
-//
-//                R.id.navAdd -> replaceFragment(AddFragment())
-//                R.id.navHome -> replaceFragment(HomeFragment())
-//                R.id.navUser -> replaceFragment(UserFragment())
-//
-//                else ->{}
-//            }
-//        }
         setContentView(R.layout.activity_bottom_nav_bar)
         bottomNavigationView = findViewById(R.id.bottomNavBarID)
-        //BottomNavigationItemView = findViewById<>(R.id.bottomNavBarID)
 
         bottomNavigationView.setOnItemReselectedListener { menuItem ->
             when (menuItem.itemId) {
@@ -50,9 +33,7 @@ class BottomNavBar : AppCompatActivity() {
 
                 else -> false
 
-
             }
-
 
         }
 
@@ -62,9 +43,6 @@ class BottomNavBar : AppCompatActivity() {
 
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(R.id.frame_layout, fragment).commit()
-//        val fragmentManager = supportFragmentManager
-//        val fragmentTransaction = fragmentManager.beginTransaction()
-//        fragmentTransaction.replace(R.id.frame_layout, fragment)
-//        fragmentTransaction.commit()
+
     }
 }
